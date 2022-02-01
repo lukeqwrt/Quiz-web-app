8d1140723f7f13ae4d11d0c243602bd41012ca9f
luke <luke.adrian.vinluan.sillano@gmail.com>
2022-02-01 18:27:09 +0800

    first commit

---
+++ quiz-pages/jsquiz/js/questions.js
@@ -0,0 +1,62 @@
+//creating an array  and passing the number , question, option and answer
+let questions =[
+    {
+        numb: 1,
+        question: "In a bingo game, which number is represented by the phrase “two little ducks”?",
+        answer: "22",
+        options:[
+            "21",
+            "22",
+            "31",
+            "32"
+        ]
+    },
+    {
+        numb: 2,
+        question: "According to Greek mythology, who was the first woman on earth?",
+        answer: "Pandora",
+        options:[
+            "Pandora",
+            "Medea",
+            "Penelope",
+            "Arachne"
+        ]
+    },
+    {
+        numb: 3,
+        question: "How many of Snow White’s seven dwarfs have names ending in the letter Y?",
+        answer: "Five",
+        options:[
+            "Six",
+            "Four",
+            "Three",
+            "Five"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What is the tallest breed of dog in the world?",
+        answer: "The Great Dane",
+        options:[
+            "Scottish Deerhound",
+            "Irish Wolfhound",
+            "The Great Dane",
+            "Tamaskan Husky"
+        ]
+    },
+    {
+        numb: 5,
+        question: "How many ribs are in a human body?",
+        answer: "Twenty-four",
+        options:[
+            "Twenty-four",
+            "Twenty-two",
+            "Twenty-six",
+            "Twenty"
+        ]
+    }
+
+]
+
+
+
